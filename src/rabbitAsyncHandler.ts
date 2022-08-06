2d77d659fa1c1ebf6ff200978e2e4ca2dde04247
AdriDevelopsThings <adrianbloober@gmail.com>
2022-08-07 01:37:11 +0200

    add sentry

--- src/rabbitAsyncHandler.ts
+++ src/rabbitAsyncHandler.ts
@@ -1,4 +1,5 @@
 import { error } from './logger.js'
+import Sentry from '@sentry/node'
 
 type RabbitCallback = (msg: any, ack: (error?: any, reply?: any) => void) => void
 type InternalRabbitCallback = (msg: any) => Promise<{ error?: any, reply?: any }|void>
@@ -12,10 +13,24 @@ const rabbitAsyncHandler = (callback: InternalRabbitCallback): RabbitCallback =>
             }).catch(e => {
                 ack({ error: e.toString() })
                 error(`Error while handling (callback) rabbit message: ${msg.fields.routingKey}: ${msg.content.toString()} ${e}\n${e.stack}.`)
+                if (process.env.SENTRY_DSN) {
+                    Sentry.configureScope(scope => {
+                        scope.setExtra('handler', msg.fields.routingKey)
+                        scope.setExtra('content', JSON.parse(msg.content.toString()))
+                    })
+                    Sentry.captureException(e)
+                }
             })
         } catch (e: any) {
             ack({ error: e.toString() })
             error(`Error while handling rabbit message: ${msg.fields.routingKey}: ${msg.content.toString()}: ${e}\n${e.stack}.`)
+            if (process.env.SENTRY_DSN) {
+                Sentry.configureScope(scope => {
+                    scope.setExtra('handler', msg.fields.routingKey)
+                    scope.setExtra('content', JSON.parse(msg.content.toString()))
+                })
+                Sentry.captureException(e)
+            }
         }
     }
 }
